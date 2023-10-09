fd0e5de4e72ed3ab3bc4ab0635c37e9a5622ff03
TahaNamdar <tahanamdar30@gmail.com>
2023-10-09 17:56:18 +0330

    develop: responsive life goals page

--- app/lifeGoals/page.tsx
+++ app/lifeGoals/page.tsx
@@ -16,8 +16,8 @@ export default function LifeGoals() {
   return (
     <div className=" bg-CharlestonGreen lg:bg-darkGunmetal lg:flex md:p-1">
       <SideBar />
-      <div className="flex-1 p-10  bg-CharlestonGreen rounded-md pl-[2rem] pr-[2rem] md:pl-[3.7rem] md:pr-[4.6rem] h-screen lg:h-auto">
-        <div className="md:flex items-center justify-between mb-[3.7rem] mt-[4.5rem]">
+      <div className="flex-1  bg-CharlestonGreen rounded-md md:pl-[3.7rem] md:pr-[4.6rem] h-screen lg:h-auto">
+        <div className="lg:flex items-center justify-between mb-[1rem] lg:mb-[3.7rem] mt-[4.5rem] pl-[2rem] pr-[2rem]">
           <div className="lg:mb-[3.7rem]">
             <p className="text-3xl mb-[1.4rem]  md:text-4xl text-white font-medium md:mb-2">
               Life Goals{" "}
@@ -49,28 +49,28 @@ export default function LifeGoals() {
         </div>
         {/* tags input component */}
         <main>
-          <div className="bg-darkGunmetal lg:pr-[3.8rem] lg:pl-[3.8rem] rounded-t-[1.4rem]">
-            <div className="text-white lg:pt-[2rem] lg:pb-[1.9rem]  lg:text-[1.8rem] flex ">
-              <p className="lg:mr-[4.7rem]">Age</p>
+          <div className="bg-darkGunmetal pr-[3.8rem] pl-[2.3rem] lg:pl-[3.8rem] lg:rounded-t-[1.4rem]">
+            <div className="text-white text-2xl pt-[2rem] pb-[1.9rem]  lg:text-[1.8rem] flex ">
+              <p className="mr-[2.2rem] lg:mr-[4.7rem]">Age</p>
               <p>achievements</p>
             </div>
           </div>
           {/* chip wrapper  */}
-          <div className="bg-Crayola  lg:pr-[3.8rem] lg:pl-[3.8rem] rounded-b-[1.4rem] ">
+          <div className="bg-Crayola pr-[2rem] pl-[2rem]  lg:pr-[3.8rem] lg:pl-[3.8rem] lg:rounded-b-[1.4rem] ">
             <section className="pt-[2rem] pb-[2rem] ">
               <div className="flex items-start mb-[1.6rem]">
-                <div className="bg-darkGunmetal text-center text-white pr-[1.7rem] pl-[1.7rem] pt-[1.4rem] pb-[1.4rem] mr-[1.3rem] flex justify-center items-center  text-[1.8rem] rounded-[1.4rem]">
+                <div className="bg-darkGunmetal text-center text-white pr-[1.3rem] pl-[1.3rem] pt-[1.4rem] pb-[1.4rem] lg:pr-[1.7rem] lg:pl-[1.7rem] lg:pt-[1.4rem] lg:pb-[1.4rem] mr-[1.3rem] flex justify-center items-center text-[1.4rem]  lg:text-[1.8rem] rounded-[1.4rem]">
                   21
                 </div>
                 {/* chip  */}
-                <div className="bg-darkGunmetal rounded-[1.4rem]  w-full ">
+                <div className="bg-darkGunmetal rounded-[1.4rem] w-4/5 sm:w-full ">
                   <div className="pt-[1.2rem] pb-[0.8rem] pl-[1.8rem] relative flex">
-                    <div className="flex items-center flex-wrap w-10/12  ">
+                    <div className="flex items-center flex-wrap w-11/12 lg:w-10/12  ">
                       {tags.map((tag, index) => {
                         return (
                           <div
                             key={index}
-                            className={`bg-chipColor mb-[0.6rem] text-[1.8rem] mr-[1.6rem] text-black rounded-[3.4rem] pt-[0.2rem] pb-[0.2rem] pr-[0.8rem] pl-[0.8rem] ${
+                            className={`bg-chipColor mb-[0.6rem] text-[1.4rem] lg:text-[1.8rem] mr-[0.8rem] lg:mr-[1.6rem] text-black rounded-[3.4rem] pt-[0.2rem] pb-[0.2rem] pr-[0.8rem] pl-[0.8rem] ${
                               tag == "" ? "hidden" : "block"
                             }`}
                           >
@@ -82,11 +82,11 @@ export default function LifeGoals() {
                         type="text"
                         placeholder="Type to add a goal..."
                         onKeyUp={addTags}
-                        className="pr-[1.8rem] text-[1.8rem] bg-darkGunmetal text-placeholder focus:outline-none placeholder-placeholder mb-[0.6rem] "
+                        className="pr-[1.8rem] text-[1.4rem] lg:text-[1.8rem] w-[14rem] md:w-[unset] bg-darkGunmetal text-placeholder focus:outline-none placeholder-placeholder mb-[0.6rem] "
                       />
                     </div>
-                    <div className=" text-center flex items-center justify-center w-2/12 lg:ml-[0.6rem]">
-                      <p className="text-white">72 days left</p>
+                    <div className="hidden text-center md:flex justify-end pr-[0.8rem] md:pr-[2.2rem] md:items-center  w-2/12 lg:ml-[0.6rem]">
+                      <p className="text-white ">72 days left</p>
                     </div>
                   </div>
                 </div>
