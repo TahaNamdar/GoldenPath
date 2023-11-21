487768983f11cdf5cf6d00383aa97e82f4b18cc0
Taha Namdar <42145229+TahaNamdar@users.noreply.github.com>
2023-11-21 19:15:21 +0330

    updated google provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -27,6 +27,8 @@ export const authOptions: NextAuthOptions = {
       authorize: async (credentials, request) => {
         const creds = await loginSchema.parseAsync(credentials);
 
+        console.log(creds, "c");
+
         const user = await prisma.user.findFirst({
           where: { email: creds.email },
         });
@@ -51,6 +53,13 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
     // FacebookProvider({
     //   clientId: process.env.FACEBOOK_CLIENT_ID,
