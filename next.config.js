31ed13dd57b45ee21f9a3010c60baaebe8a33b93
mediv0 <anonymousaxis@gmail.com>
2023-11-25 20:37:34 +0330

    live dev polling for docker dev mod

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 // @type {import('next').NextConfig}
 
 module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
+    webpackDevMiddleware: (config) => {
+        // Solve compiling problem via vagrant
+        config.watchOptions = {
+            poll: 1000, // Check for changes every second
+            aggregateTimeout: 300, // delay before rebuilding
+        };
+        return config;
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ["@svgr/webpack"],
+        });
 
-    return config;
-  },
-  reactStrictMode: false, // React Strict Mode is off
+        return config;
+    },
+    reactStrictMode: false, // React Strict Mode is off
 };
