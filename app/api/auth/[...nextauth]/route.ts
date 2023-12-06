aba1aa281c200b29bd6be1c51c358bd25d4d0ffd
Taha Namdar <42145229+TahaNamdar@users.noreply.github.com>
2023-12-06 19:02:37 +0330

    deploy v01

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -15,140 +15,137 @@ import NextAuth from "next-auth";
 const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
-    secret: process.env.NEXTAUTH_SECRET,
-    providers: [
-        Credentials({
-            name: "SignIn",
-            credentials: {
-                email: {
-                    label: "Email",
-                    type: "email",
-                    placeholder: "golden@gmail.com",
-                },
-                password: { label: "Password", type: "password" },
-            },
-            authorize: async (credentials, request) => {
-                const creds = await loginSchema.parseAsync(credentials);
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    Credentials({
+      name: "SignIn",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "golden@gmail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials, request) => {
+        const creds = await loginSchema.parseAsync(credentials);
 
-                const user = await prisma.user.findFirst({
-                    where: { email: creds.email },
-                });
+        const user = await prisma.user.findFirst({
+          where: { email: creds.email },
+        });
 
-                if (!user) {
-                    throw new Error("user or password is wrong");
-                }
+        if (!user) {
+          throw new Error("user or password is wrong");
+        }
 
-                const [salt, storedHash] = user.password.split(".");
-                const hash = (await scrypt(creds.password, salt, 32)) as Buffer;
+        const [salt, storedHash] = user.password.split(".");
+        const hash = (await scrypt(creds.password, salt, 32)) as Buffer;
 
-                console.log(storedHash);
-                console.log(hash.toString("hex"));
+        if (hash.toString("hex") !== storedHash) {
+          throw new Error("user or password is wrong");
+        }
 
-                if (hash.toString("hex") !== storedHash) {
-                    throw new Error("user or password is wrong");
-                }
+        return {
+          id: user.id,
+          email: user.email,
+          password: user.password,
+        };
+      },
+    }),
 
-                return {
-                    id: user.id,
-                    email: user.email,
-                    password: user.password,
-                };
-            },
-        }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
 
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-            authorization: {
-                params: {
-                    prompt: "consent",
-                    access_type: "offline",
-                    response_type: "code",
-                },
-            },
-        }),
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_CLIENT_ID,
+    //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    // }),
+  ],
+  // ...
 
-        // FacebookProvider({
-        //   clientId: process.env.FACEBOOK_CLIENT_ID,
-        //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-        // }),
-    ],
-    // ...
+  jwt: {
+    secret: "super-secret",
+    maxAge: 15 * 24 * 30 * 60, // 15 days
+  },
+  session: {
+    strategy: "jwt",
+  },
 
-    jwt: {
-        secret: "super-secret",
-        maxAge: 15 * 24 * 30 * 60, // 15 days
-    },
-    session: {
-        strategy: "jwt",
-    },
+  callbacks: {
+    //...
+    async signIn({ user, account, profile, email, credentials }) {
+      if (account?.provider === "credential") {
+        return true;
+      }
 
-    callbacks: {
-        //...
-        async signIn({ user, account, profile, email, credentials }) {
-            console.log({ user, account, profile, email, credentials });
+      // it will be anything other than login with user pass
+      if (user.email) {
+        try {
+          const exists = await prisma.user.findFirst({
+            where: { email: user.email },
+          });
 
-            if(account?.provider === "credential") {
-              return true;
-            }
-
-            // it will be anything other than login with user pass
-            // if (user.email) {
-            //   const exists = await prisma.user.findFirst({
-            //     where: { email: user.email },
-            //   });
-
-            //   if (!exists) {
-            //     const result = await prisma.user.create({
-            //       data: { email: user.email, password: "", isSocialMedia: true },
-            //     });
-
-            //     const _LifeGoalDocuments = [];
-
-            //     for (let i = 1; i <= 100; i++) {
-            //       _LifeGoalDocuments.push({
-            //         userId: result.id,
-            //         age: i,
-            //         Chips: [],
-            //       });
-            //     }
-
-            //     await prisma.lifeGoals.createMany({
-            //       data: _LifeGoalDocuments,
-            //     });
-            //   }
-            // }
-
-            // const isAllowedToSignIn = true;
-            // if (isAllowedToSignIn) {
-            //   return true;
-            // } else {
-            //   // Return false to display a default error message
-            //   return false;
-            //   // Or you can return a URL to redirect to:
-            //   // return '/unauthorized'
-            // }
-
+          if (exists) {
+            user.id = exists.id;
             return true;
-        },
+          }
 
-        jwt: async ({ token, user }) => {
-            if (user) {
-                token.id = user.id;
-                token.email = user.email;
-            }
+          const result = await prisma.user.create({
+            data: { email: user.email, password: "", isSocialMedia: true },
+          });
 
-            return token;
-        },
+          user.id = result.id;
 
-        session: async ({ session, token }) => {
-            if (token) {
-                (session as any).id = token.id;
-            }
+          const _LifeGoalDocuments = [];
 
-            return session;
-        },
+          for (let i = 1; i <= 100; i++) {
+            _LifeGoalDocuments.push({
+              userId: result.id,
+              age: i,
+              Chips: [],
+            });
+          }
+
+          await prisma.lifeGoals.createMany({
+            data: _LifeGoalDocuments,
+          });
+
+          return true;
+        } catch (e) {
+          console.log(e);
+          return false;
+        }
+      }
+
+      return false;
     },
+
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+      }
+
+      return token;
+    },
+
+    session: async ({ session, token }) => {
+      if (token) {
+        (session as any).id = token.id;
+      }
+
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
